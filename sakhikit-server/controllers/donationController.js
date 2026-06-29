8b06f9c0ea64e321b503cdd00aca8461526b9526
Vinay <vinayreddaboina3574@gmail.com>
2026-06-29 23:40:30 +0530

    add Stripe donations, subscription flow, donor dashboard

--- sakhikit-server/controllers/donationController.js
+++ sakhikit-server/controllers/donationController.js
@@ -3,7 +3,9 @@ import Donation from '../models/Donation.js';
 import Cause from '../models/Cause.js';
 import NGO from '../models/NGO.js';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-10-16',
+});
 
 // POST /api/donations/create-payment-intent
 // One-time donation
